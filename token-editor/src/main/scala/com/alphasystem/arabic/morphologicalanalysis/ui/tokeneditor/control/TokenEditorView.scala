8314276f3b8c36db41bf37b61cc87b77da9e4c8f
sfali <f.syed.ali@gmail.com>
2022-10-19 21:16:56 -0400

    using properties from view

--- token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/TokenEditorView.scala
+++ token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/TokenEditorView.scala
@@ -13,7 +13,7 @@ import fx.ui.util.UiUtilities
 import javafx.application.Platform
 import javafx.scene.control.{ Control, Skin }
 import scalafx.Includes.*
-import scalafx.beans.property.{ ObjectProperty, ReadOnlyStringProperty, ReadOnlyStringWrapper, StringProperty }
+import scalafx.beans.property.{ ReadOnlyStringProperty, ReadOnlyStringWrapper }
 import scalafx.collections.ObservableBuffer
 
 import java.util.concurrent.{ Executors, TimeUnit }
@@ -24,24 +24,42 @@ class TokenEditorView(serviceFactory: ServiceFactory) extends Control {
 
   private val titlePropertyWrapper = ReadOnlyStringWrapper("")
 
-  private[control] val tokenProperty: ObjectProperty[Token] =
-    ObjectProperty[Token](this, "token")
+  private[control] val chapterVerseSelectionView =
+    ChapterVerseSelectionView(serviceFactory)
 
-  private[control] val locationsProperty: ObservableBuffer[Location] =
-    ObservableBuffer.empty[Location]
+  private[control] val tokenView = TokenView(serviceFactory)
 
-  tokenProperty.onChange((_, _, nv) => {
-    titlePropertyWrapper.value = Option(nv).map(_.displayName).getOrElse("")
-  })
+  private[control] val locationView = LocationView()
+
+  tokenView
+    .tokenProperty
+    .onChange((_, _, nv) => {
+      titlePropertyWrapper.value = Option(nv).map(_.displayName).getOrElse("")
+    })
+
+  tokenView
+    .selectedLocationProperty
+    .onChange((_, _, nv) => {
+      if Option(nv).isDefined && locationView.location != nv then locationView.location = nv
+    })
+
+  locationView
+    .locationProperty
+    .onChange((_, _, nv) => if Option(nv).isDefined then tokenView.updateLocation(nv.wordType, nv.properties))
+
+  chapterVerseSelectionView
+    .selectedTokenProperty
+    .onChange((_, _, nv) =>
+      if Option(nv).isDefined then tokenView.token = nv.userData
+      else tokenView.token = null
+    )
 
   setSkin(createDefaultSkin())
 
   def title: String = titleProperty.value
-
   def titleProperty: ReadOnlyStringProperty = titlePropertyWrapper.readOnlyProperty
 
-  override def createDefaultSkin(): Skin[_] =
-    new TokenEditorSkin(this, serviceFactory)
+  override def createDefaultSkin(): Skin[_] = TokenEditorSkin(this)
 
   def saveLocations(): Unit =
     executorService.schedule(
@@ -54,9 +72,9 @@ class TokenEditorView(serviceFactory: ServiceFactory) extends Control {
 
   private def saveLocationsInternal(): Unit = {
     UiUtilities.toWaitCursor(this)
-    val token = tokenProperty.value
-    if Option(token).isDefined && locationsProperty.nonEmpty then {
-      val service = serviceFactory.createLocations(token.toLocationRequest, locationsProperty.toList)
+    val token = tokenView.tokenProperty.value
+    if Option(token).isDefined && tokenView.locationsProperty.nonEmpty then {
+      val service = serviceFactory.createLocations(token.toLocationRequest, tokenView.locationsProperty.toList)
 
       service.onSucceeded = event => {
         UiUtilities.toDefaultCursor(this)
--- token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/skin/TokenEditorSkin.scala
+++ token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/skin/TokenEditorSkin.scala
@@ -7,25 +7,14 @@ package control
 package skin
 
 import morphology.model.WordType
-import control.{ ChapterVerseSelectionView, LocationView, TokenEditorView, TokenView }
-import service.ServiceFactory
 import javafx.scene.control.SkinBase
 import scalafx.Includes.*
-import scalafx.collections.ObservableBuffer
 import scalafx.geometry.{ Insets, Pos }
 import scalafx.scene.layout.{ BorderPane, VBox }
 
-class TokenEditorSkin(control: TokenEditorView, serviceFactory: ServiceFactory)
-    extends SkinBase[TokenEditorView](control) {
+class TokenEditorSkin(control: TokenEditorView) extends SkinBase[TokenEditorView](control) {
 
   import TokenEditorSkin.*
-
-  private val chapterVerseSelectionView =
-    ChapterVerseSelectionView(serviceFactory)
-
-  private val tokenView = TokenView(serviceFactory)
-
-  private val locationView = LocationView()
 
   getChildren.add(initializeSkin)
 
@@ -38,46 +27,10 @@ class TokenEditorSkin(control: TokenEditorView, serviceFactory: ServiceFactory)
       padding = Insets(Gap, Gap, Gap, Gap)
     }
 
-    control.tokenProperty.bind(tokenView.tokenProperty)
-
-    tokenView
-      .locationsProperty
-      .onChange((_, changes) =>
-        changes.foreach {
-          case ObservableBuffer.Add(_, added) =>
-            if added.nonEmpty then {
-              control.locationsProperty.removeAll(added.toSeq*)
-              control.locationsProperty.addAll(added)
-            }
-
-          case ObservableBuffer.Remove(_, removed) =>
-            if removed.nonEmpty then control.locationsProperty.removeAll(removed.toSeq*)
-
-          case _ => ()
-        }
-      )
-
-    tokenView
-      .selectedLocationProperty
-      .onChange((_, _, nv) => {
-        if Option(nv).isDefined && locationView.location != nv then locationView.location = nv
-      })
-
-    locationView
-      .locationProperty
-      .onChange((_, _, nv) => if Option(nv).isDefined then tokenView.updateLocation(nv.wordType, nv.properties))
-
-    vBox.getChildren.addAll(chapterVerseSelectionView, tokenView, locationView)
+    vBox.getChildren.addAll(control.chapterVerseSelectionView, control.tokenView, control.locationView)
 
     pane.center = vBox
     BorderPane.setAlignment(vBox, Pos.Center)
-
-    chapterVerseSelectionView
-      .selectedTokenProperty
-      .onChange((_, _, nv) =>
-        if Option(nv).isDefined then tokenView.token = nv.userData
-        else tokenView.token = null
-      )
 
     pane
   }
@@ -86,6 +39,5 @@ class TokenEditorSkin(control: TokenEditorView, serviceFactory: ServiceFactory)
 object TokenEditorSkin {
   private val Gap = 10.0
 
-  def apply(control: TokenEditorView, serviceFactory: ServiceFactory) =
-    new TokenEditorSkin(control, serviceFactory)
+  def apply(control: TokenEditorView): TokenEditorSkin = new TokenEditorSkin(control)
 }
