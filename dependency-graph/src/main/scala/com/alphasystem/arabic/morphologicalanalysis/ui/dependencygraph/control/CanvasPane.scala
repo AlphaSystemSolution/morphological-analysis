a514c33fdfe4bb39187689b03d4ecaa667785e89
sfali <f.syed.ali@gmail.com>
2022-10-24 18:40:26 -0400

    background colors

--- dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/CanvasPane.scala
+++ dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/CanvasPane.scala
@@ -5,22 +5,29 @@ package ui
 package dependencygraph
 package control
 
+import fx.ui.util.*
 import scalafx.Includes.*
 import scalafx.scene.layout.{ Pane, Region }
+import scalafx.scene.paint.Color
 import scalafx.stage.Screen
 
 class CanvasPane extends Pane {
+
+  private val bounds = Screen.primary.visualBounds
 
   private val canvasPane = new Pane() {
     minWidth = Region.USE_PREF_SIZE
     minHeight = Region.USE_PREF_SIZE
     maxWidth = Region.USE_PREF_SIZE
     maxHeight = Region.USE_PREF_SIZE
-    private val bounds = Screen.primary.visualBounds
     prefWidth = bounds.width
-    prefHeight = bounds.height
+    prefHeight = 600
+    style = s"-fx-background-color: ${Color.Beige.toHex}"
   }
 
+  prefWidth = bounds.width
+  prefHeight = bounds.height
+  style = s"-fx-background-color: ${Color.White.toHex}"
   children = Seq(canvasPane)
 
 }
