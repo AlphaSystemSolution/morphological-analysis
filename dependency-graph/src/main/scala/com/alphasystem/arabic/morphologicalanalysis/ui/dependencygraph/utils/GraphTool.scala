828e2e687676ee735628943aada2b3ffdeb1e581
sfali <f.syed.ali@gmail.com>
2022-10-29 07:53:00 -0400

    different drawing functions

--- dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/utils/GraphTool.scala
+++ dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/utils/GraphTool.scala
@@ -6,7 +6,8 @@ package dependencygraph
 package utils
 
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{ LineTo, MoveTo, Path }
+import scalafx.scene.shape.{ Circle, CubicCurve, Line, LineTo, MoveTo, Path, Polyline }
+import scalafx.scene.text.{ Font, Text, TextAlignment }
 
 object GraphTool {
 
@@ -60,4 +61,127 @@ object GraphTool {
 
     gridLines
   }
+
+  def drawLine(
+    lineId: String,
+    x1: Double,
+    y1: Double,
+    x2: Double,
+    y2: Double,
+    strokeColor: Color,
+    width: Double
+  ): Line =
+    new Line() {
+      id = lineId
+      startX = x1
+      startY = y1
+      endX = x2
+      endY = y2
+      stroke = strokeColor
+      strokeWidth = width
+    }
+
+  def drawDashedLine(
+    lineId: String,
+    x1: Double,
+    y1: Double,
+    x2: Double,
+    y2: Double,
+    strokeColor: Color,
+    width: Double
+  ): Line =
+    new Line() {
+      id = lineId
+      startX = x1
+      startY = y1
+      endX = x2
+      endY = y2
+      stroke = strokeColor
+      strokeWidth = width
+      fill = strokeColor
+      strokeDashArray = Seq(2.0)
+    }
+
+  def drawText(
+    textId: String,
+    value: String,
+    x1: Double,
+    y1: Double,
+    alignment: TextAlignment,
+    fillColor: Color,
+    textFont: Font
+  ): Text =
+    new Text() {
+      id = textId
+      text = value
+      textAlignment = alignment
+      fill = fillColor
+      font = textFont
+      if x1 > 0 then x = x1
+      if y1 > 0 then y = y1
+    }
+
+  def drawCircle(circleId: String, cx: Double, cy: Double, r: Double, color: Color): Circle =
+    new Circle() {
+      id = circleId
+      centerX = cx
+      centerY = cy
+      radius = r
+      stroke = color
+      fill = color
+    }
+
+  def drawCubicCurve(
+    curveId: String,
+    x: Double,
+    y: Double,
+    x1: Double,
+    y1: Double,
+    x2: Double,
+    y2: Double,
+    ex: Double,
+    ey: Double,
+    color: Color
+  ): CubicCurve =
+    new CubicCurve() {
+      id = curveId
+      startX = x
+      startY = y
+      controlX1 = x1
+      controlY1 = y1
+      controlX2 = x2
+      controlY2 = y2
+      endX = ex
+      endY = ey
+      stroke = color
+      strokeWidth = 1.0
+      fill = Color.Transparent
+    }
+
+  def drawPolyline(color: Color, points: Double*): Polyline = {
+    val polyline = Polyline(points*)
+    polyline.stroke = color
+    polyline.fill = color
+    polyline.strokeWidth = 1.0
+    polyline
+  }
+
+  def drawCubicCurveBounds(
+    startX: Double,
+    startY: Double,
+    controlX1: Double,
+    controlY1: Double,
+    controlX2: Double,
+    controlY2: Double,
+    endX: Double,
+    endY: Double
+  ): Path =
+    new Path() {
+      elements =
+        Seq(MoveTo(startX, startY), LineTo(controlX1, controlY1), LineTo(controlX2, controlY2), LineTo(endX, endY))
+      stroke = Color.Red
+      strokeWidth = 0.5
+      strokeDashArray = Seq(2.0)
+    }
+
 }
