7f0cf341e7c8927e23feef68e16b1e9113b77d03
sfali <f.syed.ali@gmail.com>
2022-11-08 21:33:20 -0500

    graph repository model

--- persistence-svc/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/morphology/persistence/model/models.scala
+++ persistence-svc/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/morphology/persistence/model/models.scala
@@ -5,37 +5,27 @@ package morphology
 package persistence
 package model
 
-import morphology.model.PartOfSpeechType
-
 trait AbstractLifted {
   def id: String
   def document: String
 }
-case class PropertiesLifted(
+
+case class LocationLifted(id: String, tokenId: String, document: String) extends AbstractLifted
+
+case class TokenLifted(id: String, verseId: String, document: String) extends AbstractLifted
+
+case class VerseLifted(id: String, chapterId: String, document: String) extends AbstractLifted
+
+case class ChapterLifted(id: String, document: String) extends AbstractLifted
+
+case class DependencyGraphLifted(
   id: String,
-  locationId: String,
+  chapterNumber: Int,
+  verseNumber: Int,
+  startTokenNumber: Int,
+  endTokenNumber: Int,
+  text: String,
   document: String)
     extends AbstractLifted
 
-case class LocationLifted(
-  id: String,
-  tokenId: String,
-  document: String)
-    extends AbstractLifted
-
-case class TokenLifted(
-  id: String,
-  verseId: String,
-  document: String)
-    extends AbstractLifted
-
-case class VerseLifted(
-  id: String,
-  chapterId: String,
-  document: String)
-    extends AbstractLifted
-
-case class ChapterLifted(
-  id: String,
-  document: String)
-    extends AbstractLifted
+case class GraphNodeLifted(id: String, graphId: String, graphNodeType: String, document: String) extends AbstractLifted
