df87382ecc0669b3fda90248107cbf5bcc3cb79d
sfali <f.syed.ali@gmail.com>
2022-12-16 07:38:19 -0500

    select settings pane

--- dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/skin/DependencyGraphSkin.scala
+++ dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/skin/DependencyGraphSkin.scala
@@ -86,6 +86,7 @@ class DependencyGraphSkin(control: DependencyGraphView) extends SkinBase[Depende
             ("Properties:", terminalNodeView, true)
           }
 
+        accordion.expandedPane = allPanes.head
         propertiesEditorView.text = text
         propertiesEditorView.content = content
         propertiesEditorView.disable = disable
