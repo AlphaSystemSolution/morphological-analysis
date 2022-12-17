905184edddf55c57a2664341c1b208c6e4f5fad9
sfali <f.syed.ali@gmail.com>
2022-12-17 08:18:54 -0500

    extract functions

--- token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/skin/VerbPropertiesSkin.scala
+++ token-editor/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/tokeneditor/control/skin/VerbPropertiesSkin.scala
@@ -6,14 +6,13 @@ package tokeneditor
 package control
 package skin
 
-import tokeneditor.*
-import control.VerbPropertiesView
-import morphology.model.{ VerbPartOfSpeechType, VerbType, NumberType, GenderType, ConversationType, VerbMode }
+import morphologicalanalysis.morphology.model.*
+import ui.tokeneditor.*
 import ui.{ ArabicSupportEnumComboBox, ListType }
 import javafx.scene.control.SkinBase
 import scalafx.Includes.*
 import scalafx.collections.ObservableBuffer
-import scalafx.geometry.Insets
+import scalafx.geometry.{ Insets, Pos }
 import scalafx.scene.control.Label
 import scalafx.scene.layout.{ BorderPane, GridPane }
 
@@ -21,82 +20,84 @@ class VerbPropertiesSkin(control: VerbPropertiesView) extends SkinBase[VerbPrope
 
   getChildren.add(initializeSkin)
 
-  import VerbPropertiesSkin.*
-
   private def initializeSkin = {
-    val borderPane = new BorderPane()
 
     val gridPane = new GridPane()
     gridPane.styleClass = ObservableBuffer("border")
-    gridPane.vgap = Gap
-    gridPane.hgap = Gap
-    gridPane.padding = Insets(Gap, Gap, Gap, Gap)
+    gridPane.vgap = DefaultGap
+    gridPane.hgap = DefaultGap
+    gridPane.padding = Insets(DefaultGap, DefaultGap, DefaultGap, DefaultGap)
 
-    val skin = getSkinnable
+    addTypeComboBox(gridPane)
+    addNumberComboBox(gridPane)
+    addGenderComboBox(gridPane)
+    addConversationTypeComboBox(gridPane)
+    addVerbModeComboBox(gridPane)
 
+    val borderPane = new BorderPane() {
+      center = gridPane
+    }
+    BorderPane.setAlignment(gridPane, Pos.Center)
+    borderPane
+  }
+
+  private def addTypeComboBox(gridPane: GridPane): Unit = {
     gridPane.add(Label("Type:"), 0, 0)
-    val verbTypeComboBox =
+    val comboBox =
       ArabicSupportEnumComboBox(
         VerbType.values,
         ListType.LABEL_AND_CODE
       )
-    verbTypeComboBox.valueProperty().bindBidirectional(skin.verbTypeProperty)
-    gridPane.add(verbTypeComboBox, 1, 0)
+    comboBox.valueProperty().bindBidirectional(control.verbTypeProperty)
+    gridPane.add(comboBox, 1, 0)
+  }
 
+  private def addNumberComboBox(gridPane: GridPane): Unit = {
     gridPane.add(Label("Number:"), 0, 1)
-    val numberTypeComboBox = ArabicSupportEnumComboBox(
-      NumberType.values,
-      ListType.LABEL_AND_CODE
-    )
-    numberTypeComboBox
-      .valueProperty()
-      .bindBidirectional(skin.numberTypeProperty)
-    gridPane.add(numberTypeComboBox, 1, 1)
+    val comboBox =
+      ArabicSupportEnumComboBox(
+        NumberType.values,
+        ListType.LABEL_AND_CODE
+      )
+    comboBox.valueProperty().bindBidirectional(control.numberTypeProperty)
+    gridPane.add(comboBox, 1, 1)
+  }
 
+  private def addGenderComboBox(gridPane: GridPane): Unit = {
     gridPane.add(Label("Gender:"), 0, 2)
-    val genderTypeComboBox =
+    val comboBox =
       ArabicSupportEnumComboBox(
         GenderType.values,
         ListType.LABEL_AND_CODE
       )
-    genderTypeComboBox
-      .valueProperty()
-      .bindBidirectional(skin.genderTypeProperty)
-    gridPane.add(genderTypeComboBox, 1, 2)
+    comboBox.valueProperty().bindBidirectional(control.genderTypeProperty)
+    gridPane.add(comboBox, 1, 2)
+  }
 
+  private def addConversationTypeComboBox(gridPane: GridPane): Unit = {
     gridPane.add(Label("Conversation Type:"), 0, 3)
-
-    val converstationTypeComboBox =
+    val comboBox =
       ArabicSupportEnumComboBox(
         ConversationType.values,
         ListType.LABEL_AND_CODE
       )
-    converstationTypeComboBox
-      .valueProperty()
-      .bindBidirectional(skin.conversationTypeProperty)
-    gridPane.add(converstationTypeComboBox, 1, 3)
+    comboBox.valueProperty().bindBidirectional(control.conversationTypeProperty)
+    gridPane.add(comboBox, 1, 3)
+  }
 
+  private def addVerbModeComboBox(gridPane: GridPane): Unit = {
     gridPane.add(Label("Mode:"), 0, 4)
-    val verbModeComboBox =
+    val comboBox =
       ArabicSupportEnumComboBox(
         VerbMode.values,
         ListType.LABEL_AND_CODE
       )
-    verbModeComboBox
-      .valueProperty()
-      .bindBidirectional(skin.verbModeProperty)
-    gridPane.add(verbModeComboBox, 1, 4)
-
-    borderPane.center = gridPane
-
-    borderPane
+    comboBox.valueProperty().bindBidirectional(control.verbModeProperty)
+    gridPane.add(comboBox, 1, 4)
   }
 }
 
 object VerbPropertiesSkin {
 
-  private val Gap = 10.0
-
-  def apply(control: VerbPropertiesView): VerbPropertiesSkin =
-    new VerbPropertiesSkin(control)
+  def apply(control: VerbPropertiesView): VerbPropertiesSkin = new VerbPropertiesSkin(control)
 }
