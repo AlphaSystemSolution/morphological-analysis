7c37ca7dc407eb2292e5e5d0ffa163ee8ad7a89e
sfali <f.syed.ali@gmail.com>
2023-01-04 21:16:47 -0500

    make it private to persistence package

--- persistence-svc-nitrite/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/morphology/persistence/DatabaseInit.scala
+++ persistence-svc-nitrite/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/morphology/persistence/DatabaseInit.scala
@@ -4,18 +4,23 @@ package morphologicalanalysis
 package morphology
 package persistence
 
-import com.alphasystem.arabic.morphologicalanalysis.morphology.persistence.cache.CacheFactory
-import com.alphasystem.arabic.morphologicalanalysis.morphology.persistence.nitrite.DatabaseSettings
+import persistence.cache.CacheFactory
+import persistence.nitrite.DatabaseSettings
 import morphology.utils.*
 import com.typesafe.config.ConfigFactory
+
+import java.nio.file.Path
 
 trait DatabaseInit {
 
   private val config = ConfigFactory.load()
 
-  private val rootPath = System.getProperty("user.home").toPath + Seq(".morphological_analysis", "no2")
+  private[persistence] val rootPath: Path =
+    System.getProperty("user.home").toPath + Seq(".morphological_analysis", "no2")
 
-  protected val database: Database = NitriteDatabase(rootPath, DatabaseSettings(config.getConfig("app.nitrite")))
+  private[persistence] val databaseSettings: DatabaseSettings = DatabaseSettings(config.getConfig("app.nitrite"))
+
+  protected val database: Database = NitriteDatabase(rootPath, databaseSettings)
 
   protected val cacheFactory: CacheFactory = CacheFactory(database)
 }
