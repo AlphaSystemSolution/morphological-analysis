9c06eb12511d852d784622dea552d199897ef196
sfali <f.syed.ali@gmail.com>
2023-02-11 09:07:34 -0500

    moving root word and letters into morphological engine module

---
+++ morphological-engine/src/main/scala/com/alphasystem/arabic/morphologicalengine/conjugation/model/RootLetters.scala
@@ -0,0 +1,15 @@
+package com.alphasystem
+package arabic
+package morphologicalengine
+package conjugation
+package model
+
+import arabic.model.ArabicLetterType
+
+case class RootLetter(letter: ArabicLetterType, index: Int)
+
+case class RootLetters(
+  firstRadical: RootLetter,
+  secondRadical: RootLetter,
+  thirdRadical: RootLetter,
+  fourthRadical: Option[RootLetter] = None)
