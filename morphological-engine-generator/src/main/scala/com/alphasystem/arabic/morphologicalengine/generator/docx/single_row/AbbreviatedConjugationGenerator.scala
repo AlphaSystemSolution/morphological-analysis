c14de9e2b18bd77eaf7fdb77d0a8eb37ea9605b8
sfali <f.syed.ali@gmail.com>
2023-02-25 17:51:53 -0500

    single row abbreviated conjugation builder

---
+++ morphological-engine-generator/src/main/scala/com/alphasystem/arabic/morphologicalengine/generator/docx/single_row/AbbreviatedConjugationGenerator.scala
@@ -0,0 +1,94 @@
+package com.alphasystem
+package arabic
+package morphologicalengine
+package generator
+package docx
+package single_row
+
+import com.alphasystem.arabic.model.ArabicWord
+import com.alphasystem.openxml.builder.wml.table.TableAdapter
+import morphologicalengine.conjugation.model.AbbreviatedConjugation
+import generator.model.ChartConfiguration
+import org.docx4j.wml.Tbl
+
+class AbbreviatedConjugationGenerator(
+  override val chartConfiguration: ChartConfiguration,
+  abbreviatedConjugations: Seq[AbbreviatedConjugation])
+    extends ChartGenerator(chartConfiguration) {
+
+  private var totalNumberOfColumns = 0
+
+  private val tblAdapter =
+    if abbreviatedConjugations.forall(_.adverbs.isEmpty) then {
+      totalNumberOfColumns = 10
+      new TableAdapter().startTable(10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0)
+    } else {
+      totalNumberOfColumns = 11
+      new TableAdapter().startTable(10.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0, 9.0)
+    }
+
+  override protected def getChart: Tbl = {
+    // TODO: header
+    abbreviatedConjugations.foreach(buildDocument)
+    tblAdapter.getTable
+  }
+
+  private def buildDocument(abbreviatedConjugation: AbbreviatedConjugation): Unit = {
+    var columnIndex = 0
+
+    tblAdapter.startRow()
+
+    if abbreviatedConjugation.adverbs.nonEmpty then {
+      tblAdapter.addColumn(
+        columnIndex,
+        getArabicText(concatenateWithAnd(abbreviatedConjugation.adverbs.map(ArabicWord(_))))
+      )
+      columnIndex += 1
+    }
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.forbidden))
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.imperative))
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.passiveParticiple.getOrElse("")))
+    columnIndex += 1
+
+    tblAdapter.addColumn(
+      columnIndex,
+      getArabicText(concatenateWithAnd(abbreviatedConjugation.verbalNouns.map(ArabicWord(_))))
+    )
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.presentPassiveTense.getOrElse("")))
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.pastPassiveTense.getOrElse("")))
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.activeParticiple))
+    columnIndex += 1
+
+    tblAdapter.addColumn(
+      columnIndex,
+      getArabicText(concatenateWithAnd(abbreviatedConjugation.verbalNouns.map(ArabicWord(_))))
+    )
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.presentTense))
+    columnIndex += 1
+
+    tblAdapter.addColumn(columnIndex, getArabicText(abbreviatedConjugation.pastTense))
+
+    tblAdapter.endRow()
+  }
+}
+
+object AbbreviatedConjugationGenerator {
+  def apply(
+    chartConfiguration: ChartConfiguration,
+    abbreviatedConjugations: Seq[AbbreviatedConjugation]
+  ): AbbreviatedConjugationGenerator =
+    new AbbreviatedConjugationGenerator(chartConfiguration, abbreviatedConjugations)
+}
