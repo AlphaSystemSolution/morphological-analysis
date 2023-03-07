d3eca64c80cca718b273f53030e126792b4289e0
sfali <f.syed.ali@gmail.com>
2023-03-06 21:27:33 -0500

    properties for morphological chart

--- morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/MorphologicalChartView.scala
+++ morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/MorphologicalChartView.scala
@@ -4,25 +4,39 @@ package morphologicalengine
 package ui
 package control
 
-import morphologicalengine.generator.model.ConjugationTemplate
+import arabic.fx.ui.util.*
+import morphologicalengine.generator.model.{ ChartConfiguration, ConjugationTemplate }
 import skin.MorphologicalChartSkin
 import javafx.scene.control.{ Control, Skin }
-import scalafx.beans.property.ObjectProperty
+import scalafx.beans.property.{ ObjectProperty, ReadOnlyBooleanWrapper }
 
-import java.nio.file.Paths
+import java.nio.file.{ Path, Paths }
 
 class MorphologicalChartView extends Control {
 
-  private val defaultTemplate = toConjugationTemplate(Paths.get("sample.json"))
+  private val defaultTemplate = ConjugationTemplate(ChartConfiguration(), Seq.empty)
 
+  private val projectNameProperty = ObjectProperty[Option[String]](this, "projectName", None)
+  private val projectDirectoryProperty = ObjectProperty[Path](this, "projectDirectory", UserDir)
   private val conjugationTemplateProperty =
     ObjectProperty[ConjugationTemplate](this, "conjugationTemplate", defaultTemplate)
+  private val transientProjectProperty = new ReadOnlyBooleanWrapper(this, "transientProject", true)
+
+  projectNameProperty.onChange((_, _, nv) => transientProjectProperty.value = nv.isEmpty)
 
   setSkin(createDefaultSkin())
+
+  def projectName: Option[String] = projectNameProperty.value
+  def projectName_=(value: Option[String]): Unit = projectNameProperty.value = value
+
+  def projectDirectory: Path = projectDirectoryProperty.value
+  def projectDirectory_=(value: Path): Unit = projectDirectoryProperty.value = value
 
   def conjugationTemplate: ConjugationTemplate = conjugationTemplateProperty.value
   def conjugationTemplate_=(value: ConjugationTemplate): Unit =
     conjugationTemplateProperty.value = if Option(value).isEmpty then defaultTemplate else value
+
+  def transientProject: Boolean = transientProjectProperty.value
 
   override def createDefaultSkin(): Skin[_] = MorphologicalChartSkin(this)
 }
