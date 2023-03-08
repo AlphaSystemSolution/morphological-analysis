5e60cb5b81fe27bd5a994137bb10de98bda035cf
sfali <f.syed.ali@gmail.com>
2023-03-08 08:18:18 -0500

    extracting create toolbar button function

--- fx-support/src/main/scala/com/alphasystem/arabic/fx/ui/util/package.scala
+++ fx-support/src/main/scala/com/alphasystem/arabic/fx/ui/util/package.scala
@@ -3,6 +3,10 @@ package arabic
 package fx
 package ui
 
+import de.jensd.fx.glyphs.{ GlyphIcon, GlyphIcons }
+import scalafx.Includes.*
+import scalafx.scene.control.{ Button, ContentDisplay, Tooltip }
+
 import java.nio.file.{ Path, Paths }
 
 package object util {
@@ -11,4 +15,19 @@ package object util {
   val UserHome: String = System.getProperty("user.home", UserDirName)
   val UserDir: Path = Paths.get(UserDirName)
   val UserHomeDir: Path = Paths.get(UserHome)
+
+  def createToolbarButton[T <: Enum[T] with GlyphIcons, V <: GlyphIcon[T]](
+    icon: V,
+    tooltipText: String,
+    action: () => Unit
+  ): Button =
+    new Button() {
+      graphic = icon
+      contentDisplay = ContentDisplay.GraphicOnly
+      tooltip = Tooltip(tooltipText)
+      onAction = event => {
+        action()
+        event.consume()
+      }
+    }
 }
