6edc66695675e23deef480d0b424d4f366dd1b5a
sfali <f.syed.ali@gmail.com>
2023-03-13 21:33:05 -0400

    an extension function

--- morphological-engine/src/main/scala/com/alphasystem/arabic/morphologicalengine/conjugation/package.scala
+++ morphological-engine/src/main/scala/com/alphasystem/arabic/morphologicalengine/conjugation/package.scala
@@ -3,8 +3,10 @@ package arabic
 package morphologicalengine
 
 import arabic.model.{ ArabicLetter, ArabicLetterType, ArabicWord, DiacriticType }
+import morphologicalengine.conjugation.forms.NounSupport
+import morphologicalengine.conjugation.forms.noun.VerbalNoun
 import conjugation.model.OutputFormat.{ BuckWalter, Html, Unicode }
-import conjugation.model.{ OutputFormat, RootLetters }
+import conjugation.model.{ ConjugationInput, OutputFormat, RootLetters }
 
 package object conjugation {
 
@@ -69,5 +71,9 @@ package object conjugation {
         case Unicode    => src.unicode
         case Html       => src.htmlCode
         case BuckWalter => src.code
+  }
+
+  extension (src: ConjugationInput) {
+    def verbalNouns: Seq[NounSupport] = src.verbalNounCodes.flatMap(code => VerbalNoun.byCode.get(code))
   }
 }
