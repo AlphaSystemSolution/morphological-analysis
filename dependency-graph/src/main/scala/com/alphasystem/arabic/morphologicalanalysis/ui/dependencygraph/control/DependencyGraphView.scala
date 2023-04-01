73e56c4fab07356369d5f9bfb4691071afdc69ac
sfali <f.syed.ali@gmail.com>
2023-04-01 12:48:22 -0400

    Do not refresh graph

--- dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/DependencyGraphView.scala
+++ dependency-graph/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/ui/dependencygraph/control/DependencyGraphView.scala
@@ -83,6 +83,9 @@ class DependencyGraphView(serviceFactory: ServiceFactory) extends Control {
     canvasView.dependencyGraph = graph
   }
 
+  // Do not refresh graph
+  private def noLoad(dependencyGraph: DependencyGraph): Unit = ()
+
   def createGraph(): Unit = {
     UiUtilities.toWaitCursor(this)
     Platform.runLater(() =>
@@ -133,7 +136,7 @@ class DependencyGraphView(serviceFactory: ServiceFactory) extends Control {
 
   def saveGraph(): Unit =
     Platform.runLater(() =>
-      graphBuilderService.saveGraph(canvasView.dependencyGraph.copy(nodes = canvasView.graphNodes), loadGraph)
+      graphBuilderService.saveGraph(canvasView.dependencyGraph.copy(nodes = canvasView.graphNodes), noLoad)
     )
 
   def openGraph(): Unit = {
