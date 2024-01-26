b586b01448b22845875e9c19c370104587d2a59f
sfali <f.syed.ali@gmail.com>
2024-01-26 13:49:28 -0500

    Representing something has been done

---
+++ persistence-svc/src/main/scala/com/alphasystem/arabic/morphologicalanalysis/morphology/persistence/Done.scala
@@ -0,0 +1,9 @@
+package com.alphasystem
+package arabic
+package morphologicalanalysis
+package morphology
+package persistence
+
+private sealed abstract class Done extends Serializable
+
+case object Done extends Done
