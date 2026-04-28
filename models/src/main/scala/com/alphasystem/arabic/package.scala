792433ffc86b0a4392d8732d6ee74c2ddf0aee0e
sfali23 <f.syed.ali@gmail.com>
2026-04-28 10:39:48 -0400

    add KeyEncoder and KeyDecoder for ProNoun

--- models/src/main/scala/com/alphasystem/arabic/package.scala
+++ models/src/main/scala/com/alphasystem/arabic/package.scala
@@ -118,6 +118,12 @@ package object arabic {
   given ProNounEncoder: Encoder[ProNoun] =
     (a: ProNoun) => Json.fromString(a.name)
 
+  given ProNounKeyEncoder: KeyEncoder[ProNoun] =
+  (a: ProNoun) => a.name
+
+  given ProNounKeyDecoder: KeyDecoder[ProNoun] =
+  (key: String) => Try(ProNoun.valueOf(key)).toOption
+  
   given RootTypeDecoder: Decoder[RootType] =
     (c: HCursor) =>
       Try(RootType.valueOf(c.value.asString.get)) match
