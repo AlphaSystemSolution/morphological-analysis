5343a0bcafd88df5aa72cc3a3e0dcade8c4236f3
sfali23 <f.syed.ali@gmail.com>
2026-05-11 14:15:51 -0400

    Remove unused `encode`, `insertStartMarkup`, and `insertEndMarkup` methods to simplify `RowGenerator` code.

--- data-tools/src/main/scala/com/alphasystem/arabic/cli/examples/RowGenerator.scala
+++ data-tools/src/main/scala/com/alphasystem/arabic/cli/examples/RowGenerator.scala
@@ -6,7 +6,6 @@ package examples
 import arabic.model.ArabicLetterType
 
 import scala.annotation.tailrec
-import scala.collection.mutable.ListBuffer
 
 object RowGenerator {
 
@@ -55,32 +54,6 @@ object RowGenerator {
   private[examples] def processText(token: String, highlights: List[Highlight]): String = {
     val tokenInfos = token.split(Space).zipWithIndex.map { case (token, index) => TokenInfo(index, token) }.toSeq
     processHighlights(token, "", highlights.reverse, tokenInfos).trim.replaceAll(Space, NoBreakingSpace)
-  }
-
-  @tailrec
-  private def encode(tokens: ListBuffer[String], highlights: List[Highlight]): ListBuffer[String] = {
-    highlights match {
-      case Nil => tokens
-      case Highlight(
-            Token(startTokenIndex, maybeStartLocationIndex),
-            Token(endTokenIndex, maybeEndLocationIndex),
-            color
-          ) :: tail =>
-        val startLocationIndex = maybeStartLocationIndex.getOrElse(1)
-        val endLocationIndex = maybeEndLocationIndex.getOrElse(-1)
-
-        // first process the endLocationIndex and insert the end markup
-        val endIndex = endTokenIndex - 1
-        val updatedEndText = insertEndMarkup(endTokenIndex, endLocationIndex, tokens(endIndex))
-        tokens.update(endIndex, updatedEndText)
-
-        // now process the startLocationIndex and insert the start markup
-        val startIndex = startTokenIndex - 1
-        val updatedStartText = insertStartMarkup(startTokenIndex, startLocationIndex, tokens(startIndex), color)
-        tokens.update(startIndex, updatedStartText)
-
-        encode(tokens, tail)
-    }
   }
 
   @tailrec
@@ -148,23 +121,6 @@ object RowGenerator {
 
         processHighlights(remainingText, updatedResult.trim, tail, tokenInfos)
     }
-  }
-
-  private def insertStartMarkup(tokenIndex: Int, locationIndex: Int, text: String, maybeColor: Option[String]) = {
-    val tokens = text.split("").toSeq
-    val textBefore =
-      if locationIndex == 1 then getTokensWithinBound(1, -1, tokens)
-      else getTokensWithinBound(1, locationIndex - 1, tokens)
-    val textAfter = if locationIndex == 1 then "" else getTokensWithinBound(locationIndex, -1, tokens)
-    val markup = maybeColor.map(ColoredMarkupStart).getOrElse(DefaultMarkup)
-    if locationIndex == 1 then s"$markup$textBefore$textAfter" else s"$textBefore$markup$textAfter"
-  }
-
-  private def insertEndMarkup(tokenIndex: Int, locationIndex: Int, text: String) = {
-    val tokens = text.split("").toSeq
-    val textBefore = getTokensWithinBound(1, locationIndex, tokens)
-    val textAfter = if locationIndex == -1 then "" else getTokensWithinBound(locationIndex + 1, -1, tokens)
-    s"$textBefore$DefaultMarkup$textAfter"
   }
 
   private[examples] def getTokensWithinBound(start: Int, end: Int, tokens: Seq[String]): String = {
