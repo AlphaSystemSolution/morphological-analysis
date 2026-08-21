0c9722bfd8215d4834b01794abab394891fdb868
sfali23 <f.syed.ali@gmail.com>
2026-08-21 17:20:01 -0400

    Refactor: Consolidate file utilities into `arabic.utils`, clean up imports, and restructure dependencies for better modularity and clarity.

--- commons/src/main/scala/com/alphasystem/arabic/utils/package.scala
+++ commons/src/main/scala/com/alphasystem/arabic/utils/package.scala
@@ -1,9 +1,32 @@
 package com.alphasystem
 package arabic
 
+import java.nio.file.{Files, Path, Paths}
+import scala.annotation.targetName
+
 package object utils {
 
   extension (src: String) {
     def asResourceUrl: String = Thread.currentThread().getContextClassLoader.getResource(src).toExternalForm
   }
+
+  extension (src: Path) {
+    @targetName("appendAsDirectory")
+    def +(others: Seq[String]) = {
+      val path = Paths.get(src.toString, others *)
+      createDirectories(path)
+      path
+    }
+    
+
+    @targetName("appendAsFile")
+    def ->(others: String*): Path = Paths.get(src.toString, others *)
+  }
+
+  extension (src: String) {
+    def toPath: Path = Paths.get(src)
+  }
+
+  private def createDirectories(path: Path): Unit = if Files.notExists(path) then Files.createDirectories(path)
+
 }
