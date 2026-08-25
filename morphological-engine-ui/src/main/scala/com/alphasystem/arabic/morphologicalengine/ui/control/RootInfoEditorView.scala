3bd6549e192141160f937fe3ec0e7bd025653a95
sfali23 <f.syed.ali@gmail.com>
2026-08-25 16:22:55 -0400

    Add support for `verbalNouns` management in `RootInfoEditorSkin` and `RootInfoEditorView`.

--- morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/RootInfoEditorView.scala
+++ morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/RootInfoEditorView.scala
@@ -5,21 +5,29 @@ package ui
 package control
 
 import com.alphasystem.arabic.model.ArabicLetterType
-import com.alphasystem.arabic.morphologicalengine.asciidoc_generator.{RootInfo, Word}
-import com.alphasystem.arabic.morphologicalengine.conjugation.model.{NamedTemplate, RootLetters}
+import com.alphasystem.arabic.morphologicalengine.asciidoc_generator.RootInfo
+import com.alphasystem.arabic.morphologicalengine.conjugation.forms.{ Form, NounSupport }
+import com.alphasystem.arabic.morphologicalengine.conjugation.forms.noun.VerbalNoun
+import com.alphasystem.arabic.morphologicalengine.conjugation.model.{
+  ConjugationConfiguration,
+  NamedTemplate,
+  RootLetters
+}
 import com.alphasystem.arabic.morphologicalengine.ui.control.skin.RootInfoEditorSkin
-import javafx.scene.control.{Control, Skin}
-import scalafx.beans.property.{ObjectProperty, StringProperty}
-import scalafx.collections.ObservableSet
+import javafx.scene.control.{ Control, Skin }
+import scalafx.beans.property.{ ObjectProperty, StringProperty }
+import scalafx.collections.{ ObservableBuffer, ObservableSet }
 
 class RootInfoEditorView extends Control {
 
   import RootInfoEditorView.*
 
   private[control] val rootLettersProperty = ObjectProperty[RootLetters](this, "rootLetters", DefaultRootLetters)
-  private[control] val familyProperty = ObjectProperty[NamedTemplate](this, "family", NamedTemplate.FormICategoryAGroupATemplate)
+  private[control] val familyProperty =
+    ObjectProperty[NamedTemplate](this, "family", NamedTemplate.FormICategoryAGroupATemplate)
   private[control] val baseTranslationProperty = new StringProperty(this, "baseTranslation")
   private[control] val translationsProperty = ObservableSet.empty[String]
+  private[control] val verbalNounsProperty: ObservableBuffer[NounSupport] = ObservableBuffer.empty[NounSupport]
 
   setSkin(createDefaultSkin())
 
@@ -32,15 +40,36 @@ class RootInfoEditorView extends Control {
   def baseTranslation: String = baseTranslationProperty.value
   private def baseTranslation_=(value: String): Unit = baseTranslationProperty.value = value
 
+  def verbalNouns: Seq[NounSupport] = verbalNounsProperty.toSeq
+
+  familyProperty.onChange((_, _, nv) => {
+    updateVerbalNouns(nv, Seq.empty)
+  })
+
   def update(rootInfo: RootInfo): Unit = {
     rootLetters = rootInfo.rootLetters
     family = rootInfo.family
     baseTranslation = rootInfo.baseTranslation
     translationsProperty.clear()
     translationsProperty.addAll(rootInfo.translations)
+    updateVerbalNouns(family, rootInfo.verbalNounCodes.flatMap(VerbalNoun.getVerbalNouns))
   }
 
-  def toWord: Word = Word(rootLetters, family, baseTranslation, translationsProperty.toSet)
+  private def updateVerbalNouns(family: NamedTemplate, verbalNouns: Seq[NounSupport]) = {
+    verbalNounsProperty.clear()
+    var _verbalNouns = verbalNouns
+    if _verbalNouns.isEmpty then _verbalNouns = Form.fromNamedTemplate(family).verbalNouns
+    verbalNounsProperty.addAll(_verbalNouns)
+  }
+
+  def toRootInfo: RootInfo = RootInfo(
+    rootLetters = rootLetters,
+    family = family,
+    baseTranslation = baseTranslation,
+    conjugationConfiguration = ConjugationConfiguration(),
+    verbalNounCodes = verbalNouns.map(_.code),
+    translations = Seq.empty
+  )
 
   override def createDefaultSkin(): Skin[?] = RootInfoEditorSkin(this)
 }
--- morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/skin/RootInfoEditorSkin.scala
+++ morphological-engine-ui/src/main/scala/com/alphasystem/arabic/morphologicalengine/ui/control/skin/RootInfoEditorSkin.scala
@@ -5,14 +5,17 @@ package ui
 package control
 package skin
 
-import arabic.morphologicalanalysis.ui.{ ArabicSupportEnumComboBox, ListType, RootLettersPickerView }
+import arabic.morphologicalanalysis.ui.{ArabicSupportEnumComboBox, ListType, RootLettersPickerView}
 import arabic.morphologicalengine.conjugation.model.NamedTemplate
+import com.alphasystem.arabic.morphologicalengine.conjugation.forms.NounSupport
 import javafx.scene.control.SkinBase
 import scalafx.Includes.*
-import scalafx.geometry.{ Insets, Pos }
-import scalafx.scene.control.{ Button, Label, TextField }
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.{Button, Label, TextField}
 //import scalafx.scene.input.{ KeyCode, KeyCodeCombination, KeyCombination }
-import scalafx.scene.layout.{ BorderPane, GridPane, Priority, VBox }
+import scalafx.scene.layout.{ BorderPane, GridPane, HBox, Priority }
+import scalafx.collections.ObservableBuffer
+import scalafx.collections.ObservableBuffer.{ Add, Remove, Reorder }
 
 class RootInfoEditorSkin(control: RootInfoEditorView) extends SkinBase[RootInfoEditorView](control) {
 
@@ -22,6 +25,7 @@ class RootInfoEditorSkin(control: RootInfoEditorView) extends SkinBase[RootInfoE
     promptText = "Base Translation"
     prefColumnCount = 30
   }
+  private val verbalNounsPicker = VerbalNounPickerView()
 
   private val searchPanel = {
     new GridPane {
@@ -40,30 +44,76 @@ class RootInfoEditorSkin(control: RootInfoEditorView) extends SkinBase[RootInfoE
     }
   }
 
+  private val verbalNounsPanel = {
+    new GridPane {
+      hgap = 8
+      vgap = 8
+      add(new Label("Verbal Nouns:"), 0, 0)
+      add(verbalNounsPicker, 1, 0)
+
+      GridPane.setHgrow(verbalNounsPicker, Priority.Always)
+    }
+  }
+
   private val skin = {
-    val vbox = new VBox {
+    val hbox = new HBox {
       padding = Insets(12)
       spacing = 10
-      children = Seq(searchPanel)
+      children = Seq(searchPanel, verbalNounsPanel)
       style = "-fx-border-color: grey; -fx-border-width: 1px; -fx-border-radius: 4px;"
     }
 
     new BorderPane {
-      center = vbox
-      BorderPane.setAlignment(vbox, Pos.Center)
+      center = hbox
+      BorderPane.setAlignment(hbox, Pos.Center)
     }
   }
 
   getChildren.addAll(skin)
 
+  verbalNounsPicker.verbalNounsProperty.clear()
+  verbalNounsPicker.verbalNounsProperty.addAll(control.verbalNouns)
   rootLettersPicker.rootLettersProperty.bindBidirectional(control.rootLettersProperty)
   familyPicker.valueProperty().bindBidirectional(control.familyProperty)
+  bindBuffers(control.verbalNounsProperty, verbalNounsPicker.verbalNounsProperty)
 
-  control
-    .rootLettersProperty
-    .onChange((_, _, nv) => {
-      println(nv)
-    })
+  private def bindBuffers(buf1: ObservableBuffer[NounSupport], buf2: ObservableBuffer[NounSupport]): Unit = {
+    var updating = false
+
+    buf1.onChange { (source, changes) =>
+      if !updating then {
+        updating = true
+        try {
+          changes.foreach {
+            case Add(position, added)      => buf2.insertAll(position, added)
+            case Remove(position, removed) => buf2.remove(position, removed.size)
+            case Reorder(from, to, perm)   =>
+              // Handle reordering if needed
+              buf2.setAll(source)
+            case _ => buf2.setAll(source)
+          }
+        } finally {
+          updating = false
+        }
+      }
+    }
+
+    buf2.onChange { (source, changes) =>
+      if !updating then {
+        updating = true
+        try {
+          changes.foreach {
+            case Add(position, added)      => buf1.insertAll(position, added)
+            case Remove(position, removed) => buf1.remove(position, removed.size)
+            case Reorder(from, to, perm)   => buf1.setAll(source)
+            case _                         => buf1.setAll(source)
+          }
+        } finally {
+          updating = false
+        }
+      }
+    }
+  }
 }
 
 object RootInfoEditorSkin {
