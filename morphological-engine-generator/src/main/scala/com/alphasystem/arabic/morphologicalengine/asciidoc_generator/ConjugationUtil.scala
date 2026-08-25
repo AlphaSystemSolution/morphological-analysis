7961df490826898703ab731a855268d061e60b5a
sfali23 <f.syed.ali@gmail.com>
2026-08-25 08:33:32 -0400

    Refactor: Enhance `ConjugationUtil` to manage file generation, improve modularity, and streamline handling of generated documents

--- morphological-engine-generator/src/main/scala/com/alphasystem/arabic/morphologicalengine/asciidoc_generator/ConjugationUtil.scala
+++ morphological-engine-generator/src/main/scala/com/alphasystem/arabic/morphologicalengine/asciidoc_generator/ConjugationUtil.scala
@@ -3,16 +3,41 @@ package arabic
 package morphologicalengine
 package asciidoc_generator
 
-import com.alphasystem.arabic.morphologicalengine.conjugation.model.ConjugationInput
-import com.alphasystem.arabic.morphologicalengine.generator.model.{ ChartConfiguration, ConjugationTemplate }
-import com.alphasystem.arabic.utils.*
+import morphologicalengine.conjugation.model.{ConjugationInput, NamedTemplate}
+import morphologicalengine.generator.model.{ChartConfiguration, ConjugationTemplate}
+import arabic.utils.*
 import com.alphasystem.asciidoc.util.DocumentConverter
 
-import java.nio.file.{ Files, Path, StandardCopyOption }
-
+import java.nio.file.{Files, Path, StandardOpenOption}
+import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters.*
+import scala.util.Using
 
 object ConjugationUtil {
+
+  private val DataDirName = "data"
+
+  private val MainAttributes =
+    """
+      |:encoding: utf-8
+      |:lang: en
+      |:linkcss:
+      |:iconfont-remote!:
+      |:compact:
+      |:stylesdir: ../../include/
+      |:stylesheet: arabic.css
+      |:docinfodir: ../../include/
+      |:docinfo2:
+      |:sectids:
+      |:toc:
+      |:secnums:
+      |:last-update-label!:
+      |:includedir: ../../include/
+      |
+      |include::{includedir}/ref.adoc[]
+      |
+      |//
+      |""".stripMargin.split(System.lineSeparator()).toBuffer
 
   private val attributes =
     """// THIS FILE IS AUTO-GENERATED, DO NOT EDIT
@@ -28,32 +53,45 @@ object ConjugationUtil {
     srcDir: Path,
     otherTranslations: Seq[String] = Seq.empty
   ): Unit = {
-    val rootPath = srcDir + Seq("data", conjugationInput.rootLetters.toDirectoryName)
-
-    // saving actual input
-    val rootInfo = conjugationInput.toRootInfo(otherTranslations)
+    val rootPath = srcDir + Seq(DataDirName, conjugationInput.rootLetters.toDirectoryName)
     val family = conjugationInput.namedTemplate.name()
     val inputFilePath = rootPath -> s"$family.yaml"
+    val existingFamily = Files.exists(inputFilePath)
+
+    // save actual input
+    val rootInfo = conjugationInput.toRootInfo(otherTranslations)
     Files.writeString(inputFilePath, toYaml(rootInfo))
 
     // generate asciidoc and save
     val id = s"${conjugationInput.rootLetters.buckWalterString}_$family"
     val conjugationTemplate = ConjugationTemplate(id, ChartConfiguration(), Seq(conjugationInput))
-    val generatedAsciidocFileName = s"${family}_generated.adoc"
-    val asciidocFilePath = rootPath -> generatedAsciidocFileName
-    MorphologicalChartGenerator.buildDocument(conjugationTemplate, asciidocFilePath, attributes)
+    val generatedAsciidocFileName = s"$family.adoc"
+    val generatedAsciidocFilePath = rootPath -> generatedAsciidocFileName
+    MorphologicalChartGenerator.buildDocument(conjugationTemplate, generatedAsciidocFilePath, attributes)
 
-    //copy main.adoc and include the generated asciidoc file
-    val mainDocPath = srcDir -> "main.adoc"
-    val destPath = rootPath -> s"$family.adoc"
-    Files.copy(mainDocPath, destPath, StandardCopyOption.REPLACE_EXISTING)
-    val lines = Files.readAllLines(destPath).asScala
-    lines.append(s"include::${asciidocFilePath.getFileName}[]")
-    Files.write(destPath, lines.asJava)
+    // if this family generated the first time, then add it `main.adoc`
+    if !existingFamily then {
+      // create copy of attributes; otherwise asciidoc will complain about duplicate ids, since same family will be included twice
+      val buffer = ListBuffer[String]()
+      buffer.addAll(MainAttributes)
+      buffer.addOne("")
+      val families = listFamilies(rootPath)
+        .sortBy(_.index)
+        .flatMap(family => Seq(s"include::$family.adoc[]", ""))
 
-    // convert to HTML and save
-    val asciiDocumentInfo = DocumentConverter.convertToHtml(destPath)
-    val htmlFilePath = rootPath -> s"$family.html"
-    Files.writeString(htmlFilePath, asciiDocumentInfo.getDocumentInfo.getContent)
+      buffer.addAll(families)
+      val destPath = rootPath -> "main.adoc"
+      Files.write(destPath, buffer.asJava, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+
+      // convert to HTML and save
+      val asciiDocumentInfo = DocumentConverter.convertToHtml(destPath)
+      Files.writeString(rootPath -> "main.html", asciiDocumentInfo.getDocumentInfo.getContent)
+    }
+  }
+
+  private def listFamilies(rootPath: Path) = {
+    Using(Files.newDirectoryStream(rootPath, "*.{yaml,yml}")) { dirStream =>
+      dirStream.asScala.map(_.getFileName.toString)
+    }.toOption.map(_.toSeq).getOrElse(Seq.empty).map(_.stripSuffix(".yaml")).map(NamedTemplate.valueOf)
   }
 }
